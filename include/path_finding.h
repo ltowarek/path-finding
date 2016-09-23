69de13c47feb927439c0e780f314564fdba1a0b2
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-09-23 18:20:04 +0200

    Move methods implementation from header to source file

--- include/path_finding.h
+++ include/path_finding.h
@@ -40,51 +40,10 @@ class PathFinding {
                const unsigned char *map, const int map_width, const int map_height,
                int *output, const int output_size) const;
  private:
-  std::vector<Point> Neighbours(const unsigned char *map, const int map_width, const int map_height, const Point &point) const {
-    auto neighbours = std::vector<Point>();
-    if ((point.x + 1 < map_width) && (map[point.y * map_width + point.x + 1] == 1)) {
-      auto neighbour = point;
-      neighbour.x += 1;
-      neighbours.push_back(neighbour);
-    }
-    if ((point.x - 1 >= 0) && (map[point.y * map_width + point.x - 1] == 1)) {
-      auto neighbour = point;
-      neighbour.x -= 1;
-      neighbours.push_back(neighbour);
-    }
-    if ((point.y + 1 < map_height) && (map[(point.y + 1) * map_width + point.x] == 1)) {
-      auto neighbour = point;
-      neighbour.y += 1;
-      neighbours.push_back(neighbour);
-    }
-    if ((point.y - 1 >= 0) && (map[(point.y - 1) * map_width + point.x] == 1)) {
-      auto neighbour = point;
-      neighbour.y -= 1;
-      neighbours.push_back(neighbour);
-    }
-    return neighbours;
-  }
-
-  int Cost(const Point &p1, const Point &p2) const {
-    return std::abs(p1.x - p2.x) + std::abs(p1.y - p2.y);
-  }
-
-  int Heuristic(const Point &p1, const Point &p2) const {
-    return Cost(p1, p2);
-  }
-
-  std::vector<Point> ReconstructPath(const Point &start, const Point &target, std::unordered_map<Point, Point, PointHasher> &came_from) const {
-    std::vector<Point> path;
-    Point current = target;
-    path.push_back(current);
-    while (current != start) {
-      current = came_from[current];
-      path.push_back(current);
-    }
-    path.pop_back();
-    std::reverse(path.begin(), path.end());
-    return path;
-  }
+  std::vector<Point> Neighbours(const unsigned char *map, const int map_width, const int map_height, const Point &point) const;
+  int Cost(const Point &p1, const Point &p2) const;
+  int Heuristic(const Point &p1, const Point &p2) const;
+  std::vector<Point> ReconstructPath(const Point &start, const Point &target, std::unordered_map<Point, Point, PointHasher> &came_from) const;
 };
 }  // pathfinding
 
--- src/path_finding.cpp
+++ src/path_finding.cpp
@@ -8,7 +8,8 @@ int pathfinding::PathFinding::FindPath(const int start_x, const int start_y,
   auto target = Point{target_x, target_y};
 
   typedef std::pair<int, Point> PointWithPriority;
-  auto frontier = std::priority_queue<PointWithPriority, std::vector<PointWithPriority>, std::greater<PointWithPriority>>();
+  auto frontier =
+      std::priority_queue<PointWithPriority, std::vector<PointWithPriority>, std::greater<PointWithPriority>>();
   frontier.emplace(Heuristic(start, target), start);
 
   auto came_from = std::unordered_map<Point, Point, PointHasher>();
@@ -17,7 +18,7 @@ int pathfinding::PathFinding::FindPath(const int start_x, const int start_y,
   came_from[start] = start;
   cost_so_far[start] = 0;
 
-  while(!frontier.empty()) {
+  while (!frontier.empty()) {
     auto current = frontier.top().second;
     frontier.pop();
 
@@ -47,4 +48,57 @@ int pathfinding::PathFinding::FindPath(const int start_x, const int start_y,
   }
 
   return -1;
+}
+
+std::vector<pathfinding::Point> pathfinding::PathFinding::Neighbours(const unsigned char *map,
+                                                                     const int map_width,
+                                                                     const int map_height,
+                                                                     const Point &point) const {
+  auto neighbours = std::vector<Point>();
+  if ((point.x + 1 < map_width) && (map[point.y * map_width + point.x + 1] == 1)) {
+    auto neighbour = point;
+    neighbour.x += 1;
+    neighbours.push_back(neighbour);
+  }
+  if ((point.x - 1 >= 0) && (map[point.y * map_width + point.x - 1] == 1)) {
+    auto neighbour = point;
+    neighbour.x -= 1;
+    neighbours.push_back(neighbour);
+  }
+  if ((point.y + 1 < map_height) && (map[(point.y + 1) * map_width + point.x] == 1)) {
+    auto neighbour = point;
+    neighbour.y += 1;
+    neighbours.push_back(neighbour);
+  }
+  if ((point.y - 1 >= 0) && (map[(point.y - 1) * map_width + point.x] == 1)) {
+    auto neighbour = point;
+    neighbour.y -= 1;
+    neighbours.push_back(neighbour);
+  }
+  return neighbours;
+}
+
+int pathfinding::PathFinding::Cost(const Point &p1, const Point &p2) const {
+  return std::abs(p1.x - p2.x) + std::abs(p1.y - p2.y);
+}
+
+int pathfinding::PathFinding::Heuristic(const Point &p1, const Point &p2) const {
+  return Cost(p1, p2);
+}
+
+std::vector<pathfinding::Point> pathfinding::PathFinding::ReconstructPath(const Point &start,
+                                                                          const Point &target,
+                                                                          std::unordered_map<Point,
+                                                                                             Point,
+                                                                                             PointHasher> &came_from) const {
+  std::vector<Point> path;
+  Point current = target;
+  path.push_back(current);
+  while (current != start) {
+    current = came_from[current];
+    path.push_back(current);
+  }
+  path.pop_back();
+  std::reverse(path.begin(), path.end());
+  return path;
 }
