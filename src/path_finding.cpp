3b78be9fb57fef78d81b64fef6adae3b4001e804
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-09-23 15:16:33 +0200

    Return invalid path by default

--- src/path_finding.cpp
+++ src/path_finding.cpp
@@ -4,5 +4,5 @@ int pathfinding::PathFinding::FindPath(const int start_x, const int start_y,
                                        const int target_x, const int target_y,
                                        const unsigned char *map, const int map_width, const int map_height,
                                        int *output, const int output_size) const {
-  return 0;
+  return -1;
 }
